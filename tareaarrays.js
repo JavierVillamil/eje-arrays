d8edec6d42dbddb316dfeca96fcfd610f171ef13
Javi Villamil <nefoja@gmail.com>
2020-07-29 14:18:32 -0500

    ejercicio array

---
+++ tareaarrays.js
@@ -0,0 +1,10 @@
+console.log("hola mundo")
+
+
+var array1 = [
+    ['f', 1, 2, 3, 's', ],
+    [5, 'o', 2, 'o', false],
+    [9, 1, 't', 2, 3, ],
+    [0, 'a', 'o', 4, 9, ],
+    ['p', 0, 6, 's', true, ]
+]
